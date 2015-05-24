f5152de10f65fed8e8ce878374e48b6fec875a4e
“MariMurotanigit config --global user.name “MariMurotani <murotanimari@MurotaniMari-no-MacBook-Air.local>
2015-05-24 20:58:33 +0900

    Spring Bootの教材用

---
+++ src/main/java/demo/controller/HelloWorldController.java
@@ -0,0 +1,34 @@
+package demo.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import demo.dto.Greeting;
+
+
+@Controller
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value="/api",method=RequestMethod.GET)
+    public @ResponseBody Greeting apiWithJson(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        
+    }
+    
+    @RequestMapping(value="/html",method=RequestMethod.GET)
+    public String pageWithHtml(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    	model.addAttribute("name", name);
+    	return "page";
+        
+    }
+
+}
