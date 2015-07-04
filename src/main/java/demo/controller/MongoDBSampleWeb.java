7d132661f34ee426c1ed38388caa949f2138c895
“MariMurotanigit config --global user.name “MariMurotani <murotanimari@MurotaniMari-no-MacBook-Air.local>
2015-07-05 00:02:28 +0900

    no message

--- src/main/java/demo/controller/MongoDBSampleWeb.java
+++ src/main/java/demo/controller/MongoDBSampleWeb.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import demo.dto.Greeting;
-import demo.dto.ResponseUser;
 import demo.dto.Status;
 import demo.mongo.model.User;
 import demo.mongo.service.UserService;
