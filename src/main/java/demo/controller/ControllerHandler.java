7af7568008c77851638019299427d4fc0791ea80
Mari Murotani <muromari5123@gmail.com>
2015-08-30 21:07:10 +0900

    CSFRつづき

--- src/main/java/demo/controller/ControllerHandler.java
+++ src/main/java/demo/controller/ControllerHandler.java
@@ -4,12 +4,14 @@ import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.ui.ModelMap;
@@ -19,6 +21,8 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import demo.annotation.ScreenTrans;
+import demo.configs.Const;
+import demo.configs.ConstError;
 
 
 public class ControllerHandler implements HandlerInterceptor  {
@@ -27,10 +31,13 @@ public class ControllerHandler implements HandlerInterceptor  {
 		throws Exception {
 		System.out.println("---Before Method Execution---");
 		
-		/*if(handler instanceof HandlerMethod){
+		// リファラー判定
+		if(handler instanceof HandlerMethod){
 			//	メソッドのアノテーション取得
 			HandlerMethod handlerMethod = (HandlerMethod)handler;
 			ScreenTrans screenTrans = handlerMethod.getMethodAnnotation(ScreenTrans.class);
+			System.out.println(handlerMethod.getMethod().toString());
+			System.out.println(screenTrans);
 			
 			//	リファラー取得
 			if(screenTrans  != null && screenTrans.referer() != null){
@@ -47,8 +54,42 @@ public class ControllerHandler implements HandlerInterceptor  {
 					}
 				}
 			}
-		}*/
-
+			
+		}
+		
+		//	CSEF判定
+		final HttpServletRequest httpRequest = (HttpServletRequest) request;
+	    final HttpServletResponse httpResponse = (HttpServletResponse) response;
+	      
+		boolean error = false;
+		System.out.println(httpRequest.getMethod());
+		if (httpRequest.getMethod().startsWith("POST")) {
+			String CSRFKey = "";
+			String CSRFVal = "";
+			httpRequest.getParameterNames();
+			Enumeration e1 = httpRequest.getParameterNames();
+		    while (e1.hasMoreElements()) {
+		    	String name = (String) e1.nextElement();
+		    	if(name.startsWith(Const.CRSF_PREFIX)){
+		    		CSRFKey = name;
+		    		CSRFVal = httpRequest.getParameterValues(CSRFKey)[0];
+		    		error = true;
+		    		break;
+		    	}
+		    }
+		    
+		    HttpSession httpSession = httpRequest.getSession();
+		    Object object = httpSession.getAttribute(CSRFKey);
+		    System.out.println(object.toString() +"=="+ CSRFVal);
+		    if(object.toString().equals(CSRFVal)){
+		    	error = false;
+		    }
+			
+		}
+		if(error){
+			new Exception(ConstError.CRSF_ERR);
+		}
+		
 		return true;
 	}
 	
